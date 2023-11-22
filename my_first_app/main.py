25474a5e6609e8a36302ac6133128e2e0a820be9
Adam Jaamour <adam.jaamour@causalens.com>
2023-11-22 11:32:19 +0000

    Add theme and auto-reload

--- my_first_app/main.py
+++ my_first_app/main.py
@@ -1,13 +1,15 @@
 from dara.core import ConfigurationBuilder
-from dara.components import Heading
 
 from my_first_app.pages.classification import classification_page
 from my_first_app.pages.eda import eda_page
 
+
 # Create a configuration builder
 config = ConfigurationBuilder()
 
+# Set theme
+config.set_theme('dark')
+
 # Register pages
-# config.add_page('Hello World', Heading('Hello World!'))
 config.add_page('EDA', eda_page())
 config.add_page('Classification', classification_page())
