b1f7ae2cd17fee6028c19b9e071eef6883313fda
Adam Jaamour <adam.jaamour@causalens.com>
2023-11-22 11:17:14 +0000

    Create classification page using decisiontree and max_depth as a variable

--- my_first_app/main.py
+++ my_first_app/main.py
@@ -1,6 +1,7 @@
 from dara.core import ConfigurationBuilder
 from dara.components import Heading
 
+from my_first_app.pages.classification import classification_page
 from my_first_app.pages.eda import eda_page
 
 # Create a configuration builder
@@ -9,3 +10,4 @@ config = ConfigurationBuilder()
 # Register pages
 # config.add_page('Hello World', Heading('Hello World!'))
 config.add_page('EDA', eda_page())
+config.add_page('Classification', classification_page())
